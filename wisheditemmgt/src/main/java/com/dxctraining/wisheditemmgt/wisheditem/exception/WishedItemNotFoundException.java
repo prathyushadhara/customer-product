357df985e4bd088e901ecaef3ce5209aa3cc2f6c
prathyusha <prathyushadhara@gmail.com>
2020-09-03 17:10:49 +0530

    changes

--- wisheditemmgt/src/main/java/com/dxctraining/wisheditemmgt/wisheditem/exception/WishedItemNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.dxctraining.wisheditemmgt.wisheditem.exception;
-
-public class WishedItemNotFoundException extends RuntimeException{
-
-		public WishedItemNotFoundException(String msg) {
-			super(msg);
-		}
-		
-}
